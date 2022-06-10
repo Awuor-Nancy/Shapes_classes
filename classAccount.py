cff0b08593d1f7ba61e508519662fd4e6ea7697f
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-06-10 23:07:26 +0300

    Account commit

---
+++ classAccount.py
@@ -0,0 +1,43 @@
+class Account :
+
+        def __init__(self,name,account_num):
+         self.name = name 
+         self.account_num = account_num
+         self.balance = 0
+         self.deposits = []
+         self.withdrawals = []
+         self.transaction = 100
+
+
+        def deposit(self,amount):
+             amount = float(input("enter the amount to deposit"))
+             self.balance = self.balance + amount
+             self.deposits.append(amount)
+             return f"Dear {self.name},you have deposited {amount} in account,your current balance is {self.balance}"
+
+
+        def withdraw(self,amount):
+             if (amount >= self.balance):
+                 return f"insufficient funds"
+             
+             elif amount <= 0:
+                 return f"amount must be greater than zero"
+
+             else :
+                 self.balance -= amount
+                 self.withdrawals.append(amount)
+                 self.balance -= self.transaction 
+                 return f"Dear {self.name} you have withdrawn {amount} and your balance is {self.balance}"
+
+
+        def statement_deposit(self):
+             for x in self.deposit: 
+                print(f"dear {self.name} your statement is {x}")
+
+        def statement_withdraw(self):
+             for amount in self.withdraw:
+                print(f"Dear {self.name} your withdrawal statement is {amount}")
+
+        def current_bal(self):
+            return f"dear {self.name} your current account balance is {self.balance}"
+            
